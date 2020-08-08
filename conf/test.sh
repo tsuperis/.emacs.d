c5704f469541fa58412f32b99f3417dfe57df836
tsuperis <tsuperis@gmail.com>
2020-08-08 10:39:53 +0900

    rm

--- conf/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git filter-branch -f --env-filter "
-if [ ${GIT_AUTHOR_NAME}='t.furuse' ]; then
-  export GIT_AUTHOR_NAME='tsuperis'
-  export GIT_AUTHOR_EMAIL='tsuperis@gmail.com'
-  export GIT_COMMITTER_NAME='tsuperis'
-  export GIT_COMMITTER_EMAIL='tsuperis@gmail.com'
-fi
-"
