cbacb703cbe74f0bc9add5f41ba04375a810aaf4
tsuperis <tsuperis@gmail.com>
2020-08-08 10:23:07 +0900

    fix

---
+++ conf/40-lsp.el
@@ -0,0 +1,19 @@
+(use-package lsp-mode
+  :ensure t
+  :bind (:map lsp-mode-map
+              ("M-*" . xref-pop-marker-stack)
+              ("M-." . xref-find-definitions)
+              ("M-/" . xref-find-references))
+  :custom
+  (lsp-prefer-capf t)
+  (lsp-prefer-flymake 'flymake)
+  :commands (lsp lsp-deferred))
+
+(use-package lsp-ui
+  :ensure t
+  :after (lsp)
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  :config
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-header t))
--- conf/60-go.el
+++ conf/60-go.el
@@ -2,7 +2,15 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
+(use-package go-eldoc)
+
 (use-package go-mode
   :ensure t
+  :after (go-eldoc)
   :mode ("\\.go\\'" . go-mode)
-  :hook (go-mode . lsp-go-install-save-hooks))
+  :hook ((go-mode . lsp-go-install-save-hooks)
+         (go-mode . lsp-deferred)
+         (go-mode . go-eldoc-setup)
+         (go-mode . (lambda ()  ;; company-goを使う
+                      (set (make-local-variable 'company-backends) '(company-go))
+                      (company-mode)))))
