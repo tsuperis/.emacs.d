c84a95c1068016be2309495f59c94e016ff14c7b
tsuperis <tsuperis@gmail.com>
2020-08-16 17:04:04 +0900

    fix lsp completion

--- conf/70-lsp.el
+++ conf/70-lsp.el
@@ -1,20 +1,34 @@
 (use-package lsp-mode
-  :hook ((
-          go-mode
-          ) . lsp)
-  :bind (:map lsp-mode-map
-              ("M-*" . xref-pop-marker-stack)
-              ("M-." . xref-find-definitions)
-              ("M-/" . xref-find-references))
+  :hook
+  ((
+    go-mode
+    ) . lsp)
   :custom
   (lsp-prefer-capf t)
   (lsp-prefer-flymake 'flymake)
-  :commands (lsp lsp-deferred))
+  :commands (lsp lsp-rename))
 
 (use-package lsp-ui
-  :requires (lsp)
+  :requires (lsp-mode)
   :hook
   (lsp-mode . lsp-ui-mode)
-  :config
+  :custom
   (lsp-ui-doc-enable t)
-  (lsp-ui-doc-header t))
+  (lsp-ui-peek-enable t)
+  :bind
+  (:map lsp-mode-map
+        ("C-c d" . my/toggle-lsp-ui-doc)
+        ("C-c r" . lsp-rename)
+        ("M-." . lsp-ui-peek-find-definitions)
+        ("M-?" . lsp-ui-peek-find-references))
+  :preface
+  (defun my/toggle-lsp-ui-doc ()
+    (interactive)
+    (if lsp-ui-doc-mode
+        (progn
+          (message "Hide lsp-ui-doc")
+          (lsp-ui-doc-mode -1)
+          (lsp-ui-doc-hide))
+      (progn
+        (message "Show lsp-ui-doc")
+        (lsp-ui-doc-mode 1)))))
