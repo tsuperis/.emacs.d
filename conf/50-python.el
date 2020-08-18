2ab805173d1a31ec9fb9aa3071d7ce889dea8696
tsuperis <tsuperis@gmail.com>
2020-08-18 19:11:39 +0900

    add flycheck

--- conf/50-python.el
+++ conf/50-python.el
@@ -20,4 +20,7 @@
   (elpy-rpc-virtualenv-path 'current)
   :config
   (delete `elpy-module-highlight-indentation elpy-modules)
+  (when (require 'flycheck nil t)
+    (remove-hook 'elpy-modules 'elpy-module-flymake)
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
   )
