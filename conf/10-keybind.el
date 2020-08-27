37ee4c9f77e85789a7e6b57226f474d9a965b41f
tsuperis <tsuperis@gmail.com>
2020-08-28 00:20:29 +0900

    install hydra

--- conf/10-keybind.el
+++ conf/10-keybind.el
@@ -5,3 +5,15 @@
             ("C-t" . other-window)
             ("C-c l" . toggle-truncate-lines)
             )
+
+(use-package hydra
+ :ensure t
+ :bind
+ ([f2] . hydra-zoom/body)
+ :config
+ (defhydra hydra-zoom (:hint t)
+   "zoom"
+   ("^" text-scale-increase "in")
+   ("-" text-scale-decrease "out")
+   ("q" nil "quit"))
+ )
